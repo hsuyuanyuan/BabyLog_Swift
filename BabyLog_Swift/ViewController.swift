03e986b3d4db0ad896a57aaea46409015db974ea
hsuyuanyuan <yuanxu@Yuans-MacBook-Pro.local>
2015-06-20 00:09:30 -0700

    Initial Commit

---
+++ BabyLog_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyLog_Swift
+//
+//  Created by Yuan Xu on 6/20/15.
+//  Copyright © 2015 StreamSaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
