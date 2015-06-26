80fb1b1b7ac3a8e5acb3db1bc12f7e318904fbe2
hsuyuanyuan <yuanxu@Yuans-MacBook-Pro.local>
2015-06-26 09:53:43 -0700

    calendar view can pop out and dismiss.  but the display is problematic.  check in before swapping to another calendar control

---
+++ BabyLog_Swift/CalendarPickerViewController.swift
@@ -0,0 +1,70 @@
+//
+//  CalendarPickerViewController.swift
+//  BabyLog_Swift
+//
+//  Created by Yuan Xu on 6/26/15.
+//  Copyright (c) 2015 StreamSaga. All rights reserved.
+//
+
+import UIKit
+
+
+class CalendarPickerViewController: UIViewController, CalendarViewDelegate {
+
+    var bDatePickedFirstTime = true
+    
+    override func viewDidLoad() {
+        
+        //yxu: added calender view https://github.com/lancy98/Calendar
+
+        // todays date.
+        let date = NSDate()
+        
+        // create an instance of calendar view with
+        // base date (Calendar shows 12 months range from current base date)
+        // selected date (marked dated in the calendar)
+        let calendarView = CalendarView.instance(date, selectedDate: date)
+        calendarView.delegate = self
+        calendarView.setTranslatesAutoresizingMaskIntoConstraints(false)
+ 
+        var placeholderView = UIView(frame: CGRectMake(30, 30, view.frame.size.width - 30, view.frame.size.height - 30))
+        
+        placeholderView.addSubview(calendarView)
+        
+        // Constraints for calendar view - Fill the parent view.
+        placeholderView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[calendarView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["calendarView": calendarView]))
+        placeholderView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[calendarView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["calendarView": calendarView]))
+        self.view.addSubview(placeholderView)
+ 
+ 
+        
+
+    }
+    
+    
+    
+    // conform to calendarViewDelegate
+    func didSelectDate(date: NSDate) {
+        println("\(date.year)-\(date.month)-\(date.day)")
+        
+        
+        if bDatePickedFirstTime {
+            bDatePickedFirstTime = false
+        }else {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+
+    /* // add viewTapped method
+    
+    // add to viewDidLoad
+    //let tapRecognizer = UITapGestureRecognizer(target: self, action: "viewTapped")
+    //view.addGestureRecognizer(tapRecognizer)
+    
+    func viewTapped() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    */
+    
+}
--- BabyLog_Swift/LogTableViewController.swift
+++ BabyLog_Swift/LogTableViewController.swift
@@ -18,31 +18,37 @@ Then go to the Editor menu, and click on the Embed In submenu, and choose Naviga
 
 */
 
-//yxu: add this for calendar view: CalendarViewDelegate
+
 class LogTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // MARK: member varaibles
     var logView = UITableView()
     
     var arrLog = ["MorningPlay", "Nap", "AfternoonPlay", "AfternoonSnack"]
    
     let cellReuseId = "logCell"
     
+    
+    
+    
+    // MARK: view lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //yxu: customize the table view
+        // customize a self-defined table view. will be added as a subview
         logView.separatorStyle = UITableViewCellSeparatorStyle.SingleLineEtched //?? yxu: does not work?? separator
         logView.frame = CGRectMake(0, 50, 320, 200)
         logView.delegate = self
         logView.dataSource = self
         logView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellReuseId)
         
-        // add a 
-        var headerView = UIView(frame: CGRectMake(0, 0, logView.frame.size.width, 30))
+        // add a label to the header
+        var headerView = UIView(frame: CGRectMake(0, 0, logView.frame.size.width, 30)) //yxu: relative to parent's view's upper left corner
         var headerLabel = UILabel(frame: CGRectMake(0, 0, logView.frame.size.width, 30))
         headerLabel.text = "Header Text"
         headerLabel.textColor = UIColor.redColor()
         headerLabel.textAlignment  = NSTextAlignment.Center
+        
         headerView.addSubview(headerLabel)
         
         // add a button to the header
@@ -50,48 +56,146 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
         btnCalendar.setTitle("ShowCalendar", forState: UIControlState.Highlighted) //yxu: note: state Selected will not be reset automatically
         btnCalendar.setTitle("ClickCalendar", forState: UIControlState.Normal)
         btnCalendar.backgroundColor = UIColor.greenColor()
+        
+        btnCalendar.addTarget(self, action: "showCalendar:", forControlEvents: .TouchUpInside) // set up the control target func
+        
         headerView.addSubview(btnCalendar)
         
+        
+        // set the table view header
         logView.tableHeaderView = headerView //yxu: this is the key, do not use addSubView
         
         
-        
-        
+        // add tableview as a subview to the generic UIViewController
         self.view.addSubview(logView)
         
         
-        /*
-        //yxu: added calender https://github.com/lancy98/Calendar
-        var placeholderView = UIView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
-        // todays date.
-        let date = NSDate()
-        
-        // create an instance of calendar view with
-        // base date (Calendar shows 12 months range from current base date)
-        // selected date (marked dated in the calendar)
-        let calendarView = CalendarView.instance(date, selectedDate: date)
-        calendarView.delegate = self
-        calendarView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        placeholderView.addSubview(calendarView)
-        
-        
-        // Constraints for calendar view - Fill the parent view.
-        placeholderView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[calendarView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["calendarView": calendarView]))
-        placeholderView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[calendarView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["calendarView": calendarView]))
 
-        self.view.addSubview(placeholderView)
+    }
+    
+
+    
+    override func viewDidAppear(animated: Bool) {
+        var refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "buttonMethod") //Use a selector
+        logVC.navigationItem.leftBarButtonItem = refreshButton
+        
+        
+        // teacher's retrieveLog module
+        
+        var requestParams : [String:AnyObject] = [
+            "Id":307,
+            //"Day":"2015-06-25",
+        ]
+        
+        
+        let manager = Manager.sharedInstance
+        manager.session.configuration.HTTPAdditionalHeaders = [
+            "Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
+        
+        
+        let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+        
+        // get by date: "http://www.babysaga.cn/app/service?method=ClassSchedule.GetListSchedule"
+        // get by Id of date: "http://www.babysaga.cn/app/service?method=ClassSchedule.GetScheduleById"
+        let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.GetScheduleById", parameters: [:], encoding: .Custom({
+            (convertible, params) in
+            var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
+            mutableRequest.HTTPBody = data
+            return (mutableRequest, nil)
+        })).responseJSON() {
+            (request, response, JSON, error) in
+            
+            if error == nil {
+                println("we did get the response")
+                println(JSON) //yxu: output the unicode
+                println(request)
+                println(response)
+                println(error)
+                println((JSON as! NSDictionary)["Error"]!)
+                
+                let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
+                if statusCode  == 200 {
+                    println("Succeeded in getting the log")
+                    
+                    
+                } else {
+                    println("Failed to get response")
+                    let errStr = (JSON as! NSDictionary)["Error"] as! String
+                    
+                }
+            } else {
+                //self.displayAlert("Login failed", message: error!.description)
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        // teacher's uploadLog module
+        /*
+        var requestParams : [String:AnyObject] = [
+        "TimeBegin":"08:00",
+        "TimeEnd":"09:30",
+        "InDay":"2015-06-25",
+        "DiaryType": 1,
+        "ScheduleRemark": "This is a test",
+        "isPublish": 0
+        ]
+        
+        
+        let manager = Manager.sharedInstance
+        manager.session.configuration.HTTPAdditionalHeaders = [
+        "Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
+        
+        
+        let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+        
+        let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.InputScheduleJson", parameters: [:], encoding: .Custom({
+        (convertible, params) in
+        var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
+        mutableRequest.HTTPBody = data
+        return (mutableRequest, nil)
+        })).responseJSON() {
+        (request, response, JSON, error) in
+        
+        if error == nil {  //??yxu: error means http error. The web api error is inside JSON
+        println("we did get the response")
+        println(JSON) //yxu: output the unicode
+        println(request)
+        println(response)
+        println(error)
+        println((JSON as! NSDictionary)["Error"]!) //yxu: output Chinese: http://stackoverflow.com/questions/26963029/how-can-i-get-the-swift-xcode-console-to-show-chinese-characters-instead-of-unic
+        
+        let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
+        if statusCode  == 200 {
+        println("Succeeded in sending the log")
+        
+        
+        } else {
+        println("Failed to get response")
+        let errStr = (JSON as! NSDictionary)["Error"] as! String
+        
+        }
+        
+        
+        } else {
+        //self.displayAlert("Login failed", message: error!.description)
+        }
+        
+        
+        
+        }
+        
         */
+        
     }
     
-    /* // related to v
-    func didSelectDate(date: NSDate) {
-        println("\(date.year)-\(date.month)-\(date.day)")
-    }
-    */
     
     
-    
-    
+    // MARK: table view delegate and datasource
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1;
     }
@@ -113,126 +217,20 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     
-    override func viewDidAppear(animated: Bool) {
-        var refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "buttonMethod") //Use a selector
-        logVC.navigationItem.leftBarButtonItem = refreshButton
 
-        
-        // teacher's retrieveLog module
- 
-        var requestParams : [String:AnyObject] = [
-            "Id":307,
-            //"Day":"2015-06-25",
-        ]
-        
-        
-        let manager = Manager.sharedInstance
-        manager.session.configuration.HTTPAdditionalHeaders = [
-        "Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
-        
-        
-        let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
-        
-        // get by date: "http://www.babysaga.cn/app/service?method=ClassSchedule.GetListSchedule"
-        // get by Id of date: "http://www.babysaga.cn/app/service?method=ClassSchedule.GetScheduleById"
-        let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.GetScheduleById", parameters: [:], encoding: .Custom({
-            (convertible, params) in
-            var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
-            mutableRequest.HTTPBody = data
-            return (mutableRequest, nil)
-        })).responseJSON() {
-            (request, response, JSON, error) in
-        
-            if error == nil {
-                println("we did get the response")
-                println(JSON) //yxu: output the unicode
-                println(request)
-                println(response)
-                println(error)
-                println((JSON as! NSDictionary)["Error"]!)
-                
-                let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
-                if statusCode  == 200 {
-                println("Succeeded in getting the log")
-            
-            
-                } else {
-                    println("Failed to get response")
-                    let errStr = (JSON as! NSDictionary)["Error"] as! String
-                
-                }
-            } else {
-            //self.displayAlert("Login failed", message: error!.description)
-            }
-        }
-        
-
-        
-        
-        
-        
-        
-        // teacher's uploadLog module
-        /*
-        var requestParams : [String:AnyObject] = [
-            "TimeBegin":"08:00",
-            "TimeEnd":"09:30",
-            "InDay":"2015-06-25",
-            "DiaryType": 1,
-            "ScheduleRemark": "This is a test",
-            "isPublish": 0
-        ]
-        
-        
-        let manager = Manager.sharedInstance
-        manager.session.configuration.HTTPAdditionalHeaders = [
-            "Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
-        
-        
-        let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
-        
-        let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.InputScheduleJson", parameters: [:], encoding: .Custom({
-            (convertible, params) in
-            var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
-            mutableRequest.HTTPBody = data
-            return (mutableRequest, nil)
-        })).responseJSON() {
-            (request, response, JSON, error) in
-            
-            if error == nil {  //??yxu: error means http error. The web api error is inside JSON
-                println("we did get the response")
-                println(JSON) //yxu: output the unicode
-                println(request)
-                println(response)
-                println(error)
-                println((JSON as! NSDictionary)["Error"]!) //yxu: output Chinese: http://stackoverflow.com/questions/26963029/how-can-i-get-the-swift-xcode-console-to-show-chinese-characters-instead-of-unic
-                
-                let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
-                if statusCode  == 200 {
-                    println("Succeeded in sending the log")
     
-                    
-                } else {
-                    println("Failed to get response")
-                    let errStr = (JSON as! NSDictionary)["Error"] as! String
-
-                }
-                
-                
-            } else {
-                //self.displayAlert("Login failed", message: error!.description)
-            }
-            
- 
-            
-        }
+    // MARK: control target
+    func showCalendar(sender: UIButton) {
         
-        */
+        println("button pressed")
+        
+        let calendarPickerVC = CalendarPickerViewController()
+
+        calendarPickerVC.modalPresentationStyle = .Custom
+        presentViewController(calendarPickerVC, animated: true, completion: nil)
+        
         
     }
-    
-    
-
     
 
 }
