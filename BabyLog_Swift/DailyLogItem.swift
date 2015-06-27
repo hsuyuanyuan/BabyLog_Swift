22bfaf858a51b12f14e612354b5b6c8a6b13a99e
hsuyuanyuan <yuanxu@Yuans-MacBook-Pro.local>
2015-06-27 02:11:39 -0700

    added a class for DailyLogItem;  used SwiftyJson to convert Json to objects; show the objects in the table view

---
+++ BabyLog_Swift/DailyLogItem.swift
@@ -0,0 +1,112 @@
+//
+//  DailyLogItem.swift
+//  BabyLog_Swift
+//
+//  Created by Yuan Xu on 6/27/15.
+//  Copyright (c) 2015 StreamSaga. All rights reserved.
+//
+
+import Foundation
+
+class DailyLogItem: Printable {
+    
+    let id: Int
+    let content: String
+    let startTime: String
+    let endTime: String
+    //todo: datetime for InDay and CreatedDateTime ??
+    
+    var description: String {
+        return "id: \(id), startTime: \(startTime), endTime: \(endTime), content: \(content)\n"
+    }
+    
+    init(id: Int, content: String?, startTime: String?, endTime: String? ) {
+        self.id = id ?? 0
+        self.content = content ?? ""
+        self.startTime = startTime ?? ""
+        self.endTime = endTime ?? ""
+    }
+    
+}
+
+/*
+输出：List<ScheduleDto>
+ScheduleDto：：
+int Id 日程序号
+DateTime CreatedDateTime 建立的时间（不起什么作用）
+string Content 日程的备注说明
+string StartTime 开始时间
+string EndTime 结束时间
+DateTime InDay 日期
+int ByCreateClass 所属哪个班级的日程
+int DiaryType 日程的类型
+string EntryTime 时间段（开始时间 “-” 结束时间）
+
+
+
+ScheduleList =     (
+{
+ByCreateClass = 41;
+Content = "This is a test";
+CreatedDateTime = "/Date(1435250595933)/";
+DiaryType = 1;
+EndTime = "09:30";
+EntryTime = "08:00-09:30";
+Guid = "<null>";
+Id = 305;
+Imgfile = "<null>";
+Imgpath = "~/content/diaryimg/youxi.jpg";
+InDay = "/Date(1435161600000)/";
+Open = 0;
+Orders = 1;
+PicList =             (
+);
+StartTime = "08:00";
+Status = 0;
+},
+
+
+{
+ByCreateClass = 41;
+Content = "This is a test";
+CreatedDateTime = "/Date(1435250717757)/";
+DiaryType = 1;
+EndTime = "09:30";
+EntryTime = "08:00-09:30";
+Guid = "<null>";
+Id = 306;
+Imgfile = "<null>";
+Imgpath = "~/content/diaryimg/youxi.jpg”; //http://www.babysaga.cn//content/diaryimg/youxi.jpg
+InDay = "/Date(1435161600000)/";
+Open = 0;
+Orders = 1;
+PicList =             (
+);
+StartTime = "08:00";
+Status = 0;
+},
+
+
+{
+ByCreateClass = 41; 所属哪个班级的日程
+Content = "This is a test";
+CreatedDateTime = "/Date(1435250900590)/"; 建立的时间（不起什么作用）
+DiaryType = 1; 日程的类型
+EndTime = "09:30"; 结束时间
+EntryTime = "08:00-09:30"; 时间段（开始时间 “-” 结束时间）
+Guid = "<null>";
+Id = 307; 日程序号
+Imgfile = "<null>";
+Imgpath = "~/content/diaryimg/youxi.jpg";
+InDay = "/Date(1435161600000)/";
+Open = 0;
+Orders = 1;
+PicList =             (
+);
+StartTime = "08:00";
+Status = 0;
+}
+);
+
+
+*/
--- BabyLog_Swift/LogTableViewController.swift
+++ BabyLog_Swift/LogTableViewController.swift
@@ -24,11 +24,11 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
     // MARK: member varaibles
     var logView = UITableView()
     
-    var arrLog = ["MorningPlay", "Nap", "AfternoonPlay", "AfternoonSnack"]
+    var arrLog = ["MorningPlay", "Nap", "AfternoonPlay", "AfternoonSnack"] // toremove
    
     let cellReuseId = "logCell"
     
-    
+    var logItemsForDisplay = [DailyLogItem]()
     
     
     // MARK: view lifecycle
@@ -36,11 +36,20 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // customize the navigation bar
         var navigationBar = UINavigationBar(frame: CGRectMake(0, 20, view.frame.size.width, 44))
-
         navigationBar.pushNavigationItem(onMakeNavitem(), animated: true)
-
         self.view.addSubview(navigationBar)
+        
+        
+        // customize a self-defined table view. will be added as a subview
+        logView.separatorStyle = UITableViewCellSeparatorStyle.SingleLineEtched //?? yxu: does not work?? separator
+        logView.frame = CGRectMake(0, 70, 320, 200)
+        logView.delegate = self
+        logView.dataSource = self
+        logView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellReuseId)
+        
+        self.view.addSubview(logView)
     }
     
     func onMakeNavitem()->UINavigationItem{
@@ -111,7 +120,7 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
         //todo: add checking for the date string
         
         var requestParams : [String:AnyObject] = [
-            //"Id":307,
+            //"Id":307, 306, 305
             "Day": date, //"2015-6-25"
         ]
         
@@ -129,25 +138,33 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
             var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
             mutableRequest.HTTPBody = data
             return (mutableRequest, nil)
-        })).responseJSON() {
-            (request, response, JSON, error) in
+            })).responseJSON() {
+            (request, response, data, error) in
             
             if error == nil {
                 println("we did get the response")
-                println(JSON) //yxu: output the unicode
+                println(data) //yxu: output the unicode
                 println(request)
                 println(response)
                 println(error)
-                println((JSON as! NSDictionary)["Error"]!)
+                println((data as! NSDictionary)["Error"]!)
                 
-                let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
+                let statusCode = (data as! NSDictionary)["StatusCode"] as! Int
                 if statusCode  == 200 {
                     println("Succeeded in getting the log")
+                    
+                    // refer to: https://grokswift.com/rest-with-alamofire-swiftyjson/
+                    if let data: AnyObject = data { //yxu: check if data is nil
+                        let jsonResult = JSON(data)
+                        
+                        self.parseJsonForLogItemArray(jsonResult)
+                    }
+                    
                     
                     
                 } else {
                     println("Failed to get response")
-                    let errStr = (JSON as! NSDictionary)["Error"] as! String
+                    let errStr = (data as! NSDictionary)["Error"] as! String
                     
                 }
             } else {
@@ -155,67 +172,44 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
             }
         }
         
+    }
+    
+    // refer to: http://stackoverflow.com/questions/26672547/swift-handling-json-with-alamofire-swiftyjson
+    // refer to: http://www.raywenderlich.com/82706/working-with-json-in-swift-tutorial
+    func parseJsonForLogItemArray(result: JSON) {
         
+        if let logItemArray = result["ScheduleList"].array {
+            
+            var logItems = [DailyLogItem]()
+            
+            for logItem in logItemArray {
+                var logId: Int = logItem["Id"].int ?? 0
+                var logContent: String? = logItem["Content"].string
+                var logStartTime: String? = logItem["StartTime"].string
+                var logEndTime: String? = logItem["EndTime"].string
+                
+                var dailyLogItem = DailyLogItem(id: logId, content: logContent, startTime: logStartTime, endTime: logEndTime)
+                logItems.append(dailyLogItem)
+            }
+            
+            logItemsForDisplay = logItems
+            
+            // Make sure we are on the main thread, and update the UI.
+            dispatch_async(dispatch_get_main_queue()) { //sync or async
+                // update some UI
+                self.logView.reloadData()
+                println("updating the table view")
+            }
+            
+        }
         
-        
-        
-        
-        
-        // teacher's uploadLog module
         /*
-        var requestParams : [String:AnyObject] = [
-        "TimeBegin":"08:00",
-        "TimeEnd":"09:30",
-        "InDay":"2015-06-25",
-        "DiaryType": 1,
-        "ScheduleRemark": "This is a test",
-        "isPublish": 0
-        ]
-        
-        
-        let manager = Manager.sharedInstance
-        manager.session.configuration.HTTPAdditionalHeaders = [
-        "Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
-        
-        
-        let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
-        
-        let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.InputScheduleJson", parameters: [:], encoding: .Custom({
-        (convertible, params) in
-        var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
-        mutableRequest.HTTPBody = data
-        return (mutableRequest, nil)
-        })).responseJSON() {
-        (request, response, JSON, error) in
-        
-        if error == nil {  //??yxu: error means http error. The web api error is inside JSON
-        println("we did get the response")
-        println(JSON) //yxu: output the unicode
-        println(request)
-        println(response)
-        println(error)
-        println((JSON as! NSDictionary)["Error"]!) //yxu: output Chinese: http://stackoverflow.com/questions/26963029/how-can-i-get-the-swift-xcode-console-to-show-chinese-characters-instead-of-unic
-        
-        let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
-        if statusCode  == 200 {
-        println("Succeeded in sending the log")
-        
-        
-        } else {
-        println("Failed to get response")
-        let errStr = (JSON as! NSDictionary)["Error"] as! String
-        
+      
+        // Make sure we are on the main thread, and update the UI.
+        dispatch_sync(dispatch_get_main_queue(), {
+            self.refreshControl!.endRefreshing()
+            self.tableView.reloadData()
         }
-        
-        
-        } else {
-        //self.displayAlert("Login failed", message: error!.description)
-        }
-        
-        
-        
-        }
-        
         */
         
     }
@@ -228,13 +222,13 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrLog.count
+        return logItemsForDisplay.count
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCellWithIdentifier(cellReuseId) as! UITableViewCell
-        cell.textLabel?.text = arrLog[indexPath.row]
+        cell.textLabel?.text = logItemsForDisplay[indexPath.row].id.description
         return cell
     }
     
@@ -258,15 +252,13 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
     
     // MARK: control target
     func showCalendar(sender: UIButton) {
-        
         println("button pressed")
-        
+
         let calendarPickerVC = KeleCalendarViewController()
         calendarPickerVC.delegate = self
         
         calendarPickerVC.modalPresentationStyle = .Custom
         presentViewController(calendarPickerVC, animated: true, completion: nil)
-        
         
     }
     
@@ -274,3 +266,64 @@ class LogTableViewController: UIViewController, UITableViewDelegate, UITableView
 
 
 }
+
+
+
+
+
+// teacher's uploadLog module
+/*
+var requestParams : [String:AnyObject] = [
+"TimeBegin":"08:00",
+"TimeEnd":"09:30",
+"InDay":"2015-06-25",
+"DiaryType": 1,
+"ScheduleRemark": "This is a test",
+"isPublish": 0
+]
+
+
+let manager = Manager.sharedInstance
+manager.session.configuration.HTTPAdditionalHeaders = [
+"Token": "VhuZ18JOWjuLxyxJ" ] //todo: retrive the token and put it in the header
+
+
+let data = NSJSONSerialization.dataWithJSONObject(requestParams, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+
+let requestSchedule =  Alamofire.request(.POST, "http://www.babysaga.cn/app/service?method=ClassSchedule.InputScheduleJson", parameters: [:], encoding: .Custom({
+(convertible, params) in
+var mutableRequest = convertible.URLRequest.copy() as! NSMutableURLRequest
+mutableRequest.HTTPBody = data
+return (mutableRequest, nil)
+})).responseJSON() {
+(request, response, JSON, error) in
+
+if error == nil {  //??yxu: error means http error. The web api error is inside JSON
+println("we did get the response")
+println(JSON) //yxu: output the unicode
+println(request)
+println(response)
+println(error)
+println((JSON as! NSDictionary)["Error"]!) //yxu: output Chinese: http://stackoverflow.com/questions/26963029/how-can-i-get-the-swift-xcode-console-to-show-chinese-characters-instead-of-unic
+
+let statusCode = (JSON as! NSDictionary)["StatusCode"] as! Int
+if statusCode  == 200 {
+println("Succeeded in sending the log")
+
+
+} else {
+println("Failed to get response")
+let errStr = (JSON as! NSDictionary)["Error"] as! String
+
+}
+
+
+} else {
+//self.displayAlert("Login failed", message: error!.description)
+}
+
+
+
+}
+
+*/
